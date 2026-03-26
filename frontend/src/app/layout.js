14006dfb8ca2e449479223aa40dfc08a06d01994
Jonas Vetsch <jonas@vetsch.com>
2026-03-26 16:38:09 +0100

    Basic react and next.js project settings, add basic styling, start work on sidebar (nav)

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,15 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/Sidebar/Sidebar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -18,8 +11,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={inter.className}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        />
+      </head>
+      <body>
+        <Sidebar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,61 @@
+import styles from "./Sidebar.module.css";
+import Link from "next/link";
+
+export default function Sidebar() {
+  return (
+    <nav className={styles.sidebar}>
+      <div>
+        <h3>Ämtliplantool</h3>
+        <p>ICT-Campus</p>
+      </div>
+
+      <ul>
+        <li>
+          <Link href="/" className={styles.navigationItem}>
+            <span className="material-symbols-outlined">home</span>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/dayassignments" className={styles.navigationItem}>
+            <span className="material-symbols-outlined">calendar_month</span>
+            <p>Einteilungen</p>
+          </Link>
+        </li>
+        <h4>Admin-Bereich</h4>
+        <li>
+          <Link href="/dayassignments" className={styles.navigationItem}>
+            <span class="material-symbols-outlined">cleaning_services</span>
+            <p>Ämtlikatalog</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/dayassignments" className={styles.navigationItem}>
+            <span className="material-symbols-outlined">group</span>
+            <p>Teams & Lernende</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/dayassignments" className={styles.navigationItem}>
+            <span className="material-symbols-outlined">date_range</span>
+            <p>Monatseinteilung</p>
+          </Link>
+        </li>
+        <li>
+          <Link href="/dayassignments" className={styles.navigationItem}>
+            <span className="material-symbols-outlined">work</span>
+            <p>Arbeitstage</p>
+          </Link>
+        </li>
+      </ul>
+
+      <div className={styles.logoutArea}>
+        <div>
+          <p>Eingeloggt als</p>
+          <h4>Coach</h4>
+        </div>
+        <span className="material-symbols-outlined">logout</span>
+      </div>
+    </nav>
+  );
+}
