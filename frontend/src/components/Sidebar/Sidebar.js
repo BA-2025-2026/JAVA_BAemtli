1bd33b9b0fa16a212cf29d7e0abdae823ff82fa4
Jonas Vetsch <jonas@vetsch.com>
2026-03-27 09:09:22 +0100

    Add logical groups to side bar layout

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -9,45 +9,54 @@ export default function Sidebar() {
         <p>ICT-Campus</p>
       </div>
 
-      <ul>
-        <li>
-          <Link href="/" className={styles.navigationItem}>
-            <span className="material-symbols-outlined">home</span>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/dayassignments" className={styles.navigationItem}>
-            <span className="material-symbols-outlined">calendar_month</span>
-            <p>Einteilungen</p>
-          </Link>
-        </li>
+      <div className={styles.navGroup}>
+        <ul>
+          <li>
+            <Link href="/" className={styles.navigationItem}>
+              <span className="material-symbols-outlined">home</span>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/dayassignments" className={styles.navigationItem}>
+              <span className="material-symbols-outlined">calendar_month</span>
+              <p>Einteilungen</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.navGroup}>
         <h4>Admin-Bereich</h4>
-        <li>
-          <Link href="/dayassignments" className={styles.navigationItem}>
-            <span className="material-symbols-outlined">cleaning_services</span>
-            <p>Ämtlikatalog</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dayassignments" className={styles.navigationItem}>
-            <span className="material-symbols-outlined">group</span>
-            <p>Teams & Lernende</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dayassignments" className={styles.navigationItem}>
-            <span className="material-symbols-outlined">date_range</span>
-            <p>Monatseinteilung</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/dayassignments" className={styles.navigationItem}>
-            <span className="material-symbols-outlined">work</span>
-            <p>Arbeitstage</p>
-          </Link>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <Link href="/dayassignments" className={styles.navigationItem}>
+              <span className="material-symbols-outlined">
+                cleaning_services
+              </span>
+              <p>Ämtlikatalog</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/dayassignments" className={styles.navigationItem}>
+              <span className="material-symbols-outlined">group</span>
+              <p>Teams & Lernende</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/dayassignments" className={styles.navigationItem}>
+              <span className="material-symbols-outlined">date_range</span>
+              <p>Monatseinteilung</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/dayassignments" className={styles.navigationItem}>
+              <span className="material-symbols-outlined">work</span>
+              <p>Arbeitstage</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
 
       <div className={styles.logoutArea}>
         <div>
@@ -59,42 +68,3 @@ export default function Sidebar() {
     </nav>
   );
 }
-
-// EHER SO UMSETZEN: GRUPPIERUNG!!
-<nav className={styles.sidebar}>
-  <div className={styles.header}>
-    <h3>Ämtliplantool</h3>
-    <p>ICT-Campus</p>
-  </div>
-
-  <div className={styles.navGroup}>
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-      <li>
-        <Link href="/dayassignments">Einteilungen</Link>
-      </li>
-    </ul>
-  </div>
-
-  <div className={styles.navGroup}>
-    <h4 className={styles.groupTitle}>Admin-Bereich</h4>
-    <ul>
-      <li>
-        <Link href="/duties">Ämtlikatalog</Link>
-      </li>
-      <li>
-        <Link href="/teams">Teams & Lernende</Link>
-      </li>
-      <li>
-        <Link href="/monthly">Monatseinteilung</Link>
-      </li>
-      <li>
-        <Link href="/workdays">Arbeitstage</Link>
-      </li>
-    </ul>
-  </div>
-
-  <div className={styles.logoutArea}>...</div>
-</nav>;
