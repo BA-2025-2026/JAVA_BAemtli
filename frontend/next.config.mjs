cc23818df6ab0ce9b451582ffe9c5479450b8acc
Jonas Vetsch <jonas@vetsch.com>
2026-05-16 12:17:40 +0200

    "Start work on containerization of frontend to prepare for a ci/cd pipeline."

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: "standalone", // optimize build file for container app (smaller file size)
 };
-
-export default nextConfig;
