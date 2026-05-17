023127d18a6d96cb03881210b39cc078f4e4f844
Jonas Vetsch <jonas@vetsch.com>
2026-05-17 21:54:35 +0200

    Debug to make backend container work as Azure Container App

--- frontend/src/lib/api/index.js
+++ frontend/src/lib/api/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080/api/v1";
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL; // ENV Variable
 
 // central error handling
 async function handleResponse(response) {
